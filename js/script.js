5d59c92a598b6b8699c45cae02efd1d15e3d9ebe
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-07-12 22:42:07 +0545

    Greeting to user

--- js/script.js
+++ js/script.js
@@ -6,7 +6,17 @@ username.innerHTML = usernamefromlocalstorage + "!";
 if (username.innerHTML == "null!") {
   window.location.href = "login.html";
 }
-// const tokenfromlocalstorage = localStorage.getItem("token");
-// if (!tokenfromlocalstorage) {
-//   window.location.href = "login.html";
-// }
+setInterval(() => {
+  const element = document.getElementById("time");
+  let curr_date = new Date();
+  let hours = curr_date.getHours();
+  let min = curr_date.getMinutes();
+  if (hours >= "12" && hours < "16") {
+    element.innerHTML = "Good Afternoon";
+  } else if (hours >= "16" && hours < "24") {
+    element.innerHTML = "Good Evening";
+  } else {
+    element.innerHTML = "Good Morning";
+  }
+  // element.innerHTML = hours + ":" + min;
+}, 1000);
