d0fc2ecbaf9fac7ba764d4fa4584a99b5504f15f
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-07-12 10:49:21 +0545

    alert if input is null

--- js/login.js
+++ js/login.js
@@ -1,6 +1,10 @@
 const continueElement = document.getElementById("continue");
 continueElement.addEventListener("click", () => {
   const inputname = document.getElementById("input").value;
-  localStorage.setItem("Name", inputname);
-  window.location.href = "index.html";
+  if (inputname == "") {
+    alert("Name cannot be null");
+  } else {
+    localStorage.setItem("Name", inputname);
+    window.location.href = "index.html";
+  }
 });
