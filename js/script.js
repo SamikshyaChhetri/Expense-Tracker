7703c36bba5c06ce126f6166d0c5a8479a766303
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-07-12 10:44:50 +0545

    go to login page if name is null

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ const username = document.getElementById("UserName");
 // const userIdfromlocalstorage = localStorage.getItem("userid");
 const usernamefromlocalstorage = localStorage.getItem("Name");
 // console.log(userIdfromlocalstorage);
-username.innerHTML = usernamefromlocalstorage;
-const tokenfromlocalstorage = localStorage.getItem("token");
-if (!tokenfromlocalstorage) {
-  window.location.href = "login.js";
+username.innerHTML = usernamefromlocalstorage + "!";
+if (username.innerHTML == "null!") {
+  window.location.href = "login.html";
 }
+// const tokenfromlocalstorage = localStorage.getItem("token");
+// if (!tokenfromlocalstorage) {
+//   window.location.href = "login.html";
+// }
